9712a35289a6bcf33f86d5a22158668b03b565b4
tcman2000 <titanchen@yahoo.com>
2021-08-29 20:30:47 -0400

    Final uncorrupted implementation
    Final versions uploaded directly as local repository corrupted

--- spec/connect4_spec.rb
+++ spec/connect4_spec.rb
@@ -14,6 +14,7 @@ end
 
 #Test cases for board class
 describe Board do
+
   #Constructor test
   describe "#initialize" do
     it "creates a Board class with a 6x7 grid of 0s(representing empty)" do
@@ -39,29 +40,76 @@ describe Board do
     #display a non-empty board
     it "Displays a non-empty game board" do
       gameBoard = Board.new
+      gameBoard.add_disc(1, 1)
+      gameBoard.display
       expect(false)
     end
 
     #display a filled board
     it "Displays a filled game board" do
       gameBoard = Board.new
+      gameBoard.grid = Array.new(6) { Array.new(7) { 1 } }
+      gameBoard.display
+      expect(false)
     end
   end
 
-  describe "#addDisc" do
+  describe "#add_disc" do
     it "Drops a disc into an empty column" do
       gameBoard = Board.new
-      gameBoard.addDisc(1, 0)
+      gameBoard.add_disc(1, 0)
       expect(gameBoard.grid[5][0]).to eql(1)
       gameBoard.display
     end
 
-    it "Drops a dis into a non-empty column" do
-      
+    it "Drops a disc into a non-empty column" do
+      gameBoard = Board.new
+      gameBoard.add_disc(1, 0)
+      gameBoard.add_disc(1, 0)
+      expect(gameBoard.grid[4][0]).to eql(1)
+      gameBoard.display
     end
 
     it "Drops a disc into a filled column" do
-      
+      gameBoard = Board.new
+      for i in 0..6
+        gameBoard.add_disc(1, 0)
+      end
+      gameBoard.display
+    end
+
+    it "Drops disc and results in a horizontal win" do
+      gameBoard = Board.new
+      for i in 0...4
+        gameBoard.add_disc(1, i)
+      end
+      gameBoard.display
+      expect(gameBoard.winner?(1)).to eql(true)
+    end
+
+    it "Drops a disc and results in a vertical win" do
+      gameBoard = Board.new
+      for i in 0...4
+        gameBoard.add_disc(1, 0)
+      end
+      gameBoard.display
+      expect(gameBoard.winner?(1)).to eql(true)
+    end
+
+    it "Drops a disc and results in a diagonal win" do
+      gameBoard = Board.new
+      gameBoard.add_disc(2, 0)
+      gameBoard.add_disc(2, 0)
+      gameBoard.add_disc(2, 0)
+      gameBoard.add_disc(2, 1)
+      gameBoard.add_disc(2, 1)
+      gameBoard.add_disc(2, 2)
+      gameBoard.add_disc(1, 0)
+      gameBoard.add_disc(1, 1)
+      gameBoard.add_disc(1, 2)
+      gameBoard.add_disc(1, 3)
+      gameBoard.display
+      expect(gameBoard.winner?(1)).to eql(true)
     end
   end
 
